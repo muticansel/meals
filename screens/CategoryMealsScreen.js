913a43651de99c079506642de78376b75aa4072a
muti <cancel.muti@gmail.com>
2020-01-14 10:35:41 +0100

    First Commit

---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet, Platform } from 'react-native';
+import Colors from '../constants/colors';
+
+import { CATEGORIES } from '../data/dummy-data';
+
+const CategoryMealScreen = props => {
+  const catId = props.navigation.getParam('categoryId');
+
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId)
+
+  return (
+    <View style={styles.screen}>
+      <Text>The Category Meal Screen!</Text>
+      <Text>{selectedCategory.title}</Text>
+      <Button title="Go to Meal Detail!" onPress={() => {
+        props.navigation.push('MealDetail')
+      }} />
+      <Button title="Go Back" onPress={() => {
+        props.navigation.goBack()
+      }} />
+    </View>
+  );
+};
+
+CategoryMealScreen.navigationOptions = (navigationData) => {
+  const catId = navigationData.navigation.getParam('categoryId');
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
+  return {
+    headerTitle: selectedCategory.title,
+    headerStyle: {
+      backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+  }
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+export default CategoryMealScreen;
